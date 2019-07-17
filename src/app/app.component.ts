e7db87f88f35309efc6483a092a2aa75b8179232
Maria Victoria <mariavictoria@MacBook-Pro-de-Maria.local>
2019-07-17 16:54:03 -0300

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AgendamentosPage } from '../pages/agendamentos/agendamentos';
+import { LoginPage } from '../pages/login/login';
+import { PerfilPage } from '../pages/perfil/perfil';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage = LoginPage;
+
+  public paginas = [
+    { titulo: 'Agendamentos', componente: AgendamentosPage },
+    { titulo: 'Perfil', componente: PerfilPage }
+  ];
+
+  @ViewChild(Nav) public nav: Nav;
+
+  constructor(
+    platform: Platform,
+    public splashscreen: SplashScreen,
+    public statusBar: StatusBar) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashscreen.hide();
+    });
+  }
+
+  abrePagina(pagina) {
+
+    this.nav.push(pagina.componente);
+  }
+}
